c6c5b9b935659fd4961c085ec13d4806efe6ea32
nahomzoe <49955806+nahomzoe@users.noreply.github.com>
2021-08-16 18:59:14 +0300

    Update shoppingList.js

---
+++ shoppingList.js
@@ -0,0 +1,33 @@
+const shoppingList = 
+[
+    {
+    name: 'Milk',
+    quantity: 5,
+    checked: true
+    },
+    {
+    name: 'Suger',
+    quantity: 1,
+    checked: false
+    },
+    {
+    name: 'Salt',
+    quantity: 1,
+    checked: True
+    },
+    {
+    name: 'Oil',
+    quantity: 3,
+    checked: false
+    },
+    {
+    name: 'Pizza',
+    quantity: 9,
+    checked: true
+    },
+    {
+    name: 'Meat',
+    quantity: 2,
+    checked: false
+    }
+]
